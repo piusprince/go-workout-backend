8f17c357a57da2d0076a60adb3933dd2e254c2c7
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-04-06 19:37:46 +0000

    feat: add MigrateFS function for filesystem-based migrations

--- internal/store/database.go
+++ internal/store/database.go
@@ -3,6 +3,7 @@ package store
 import (
 	"database/sql"
 	"fmt"
+	"io/fs"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/pressly/goose/v3"
@@ -18,6 +19,25 @@ func Open() (*sql.DB, error) {
 	return db, nil
 }
 
+func MigrateFS(db *sql.DB, migrationFS fs.FS, dir string) error {
+	goose.SetBaseFS(migrationFS)
+	defer func() {
+		goose.SetBaseFS(nil)
+	}()
+
+	return Migrate(db, dir)
+}
+
 func Migrate(db *sql.DB, dir string) error {
 	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("failed to set dialect: %w", err)
+	}
+
+	err = goose.Up(db, dir)
+	if err != nil {
+		return fmt.Errorf("migration failed: %w", err)
+	}
+
+	return nil
 }
